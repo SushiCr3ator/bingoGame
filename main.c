9896afb5a672bd9226e69905eb357ab197aceaf0
SushiCr3ator <andreazkurt.prado@gmail.com>
2025-12-03 22:17:44 +0100

    updated mistakes and some redundant lines

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 # define maxSize 75
 int balls[maxSize] = {};
-int players,currSize,currPos,currPlayCount;
+int players,currSize,currPlayCount;
 int maxPlayCount = maxSize-1;
 
 int _start() {
@@ -16,26 +16,19 @@ int _start() {
         while ((c = getchar()) != '\n' && c != EOF) {}//removes the chars in value or else the scanf will infinitly fail
     }
     return value;
-}//maybe do smth with dynamic arrays for the player bingo cards
-
-int _getRandomNum() {//creates a random number using the seed
-    currPos = rand() % currSize + 1;//= random of max amount '+' min value
-    return balls[currPos];
 }
 
-void deletePos(int pos) {
+int _getRandomNum() {//creates a random number using the seed
+    int currPos = rand() % currSize;//= random of max amount '+' min value
+    return currPos;
+}
 
-    if (pos < 0 || pos >= currSize) {
-        //basically stops the deletion if its not even in array
-        return;
-    }
-    for (int i = pos; i < currSize - 1; i++) {
+void deletePos(int arr[], int pos) {
+    for (int i = pos; i < currSize-1; i++) {
         //replaces the currPos witht the prev pos'
-        balls[i] = balls[i + 1];
+        arr[i] = arr[i + 1];
     }
     currSize--;//and srinks the array to simulate deletion
-
-
 }
 
 void _cardGenerator(int total) {//redundant 2D array but will use for smth else soon
@@ -47,7 +40,7 @@ void _cardGenerator(int total) {//redundant 2D array but will use for smth else 
         int card[5][5] ={};
         for (int col = 0; col < 5;col++) {
             for (int rows = 0; rows < 5;rows++) {
-                card[rows][col] = _getRandomNum();
+                card[rows][col] = balls[_getRandomNum()];
                 fprintf(printCard,"%d |",card[rows][col]);
             }
             fprintf(printCard,"\n");
@@ -59,11 +52,11 @@ void _cardGenerator(int total) {//redundant 2D array but will use for smth else 
 
 void _init() {//initializes the balls
     for (int i = 0; i< maxSize; i++) {
-        balls[i] = i;
-        //printf("%d\n",maxBalls[i]);
+        balls[i] = i+1;
+        //printf("%d\n",balls[i]);
     }
     currSize = maxSize;
-    _cardGenerator(_start());
+    _cardGenerator(_start());//gen amount of cards depending on how much players are given
     FILE *printHistory;
     printHistory = fopen("historyText.txt", "a");
         fprintf(printHistory, "Started BINGO game:\n");
@@ -89,25 +82,24 @@ void _historyCloser() {
 }
 
 void _round() {
-    if (currPlayCount < maxPlayCount) {
+    if (currPlayCount <= maxPlayCount) {
         currPlayCount++;
         int givenNum = _getRandomNum();
-        printf("%d\n", givenNum);
-        _history(givenNum);
-        deletePos(currPos);
+        printf("%d\n", balls[givenNum]);
+        _history(balls[givenNum]);
+        deletePos(balls,givenNum);
     }else
         printf("you have finished all the BINGO balls!!!");
 }
 
 void _playerInput() {
         char next;
-        do {
-            next = '\000';
+        do {//while (next != 'c')
             printf("\nEnter 'c' to continue to the next round; else write 'e'...");
             scanf(" %c", &next);//there should be a space before %c so that the scanf will skip anything unwanted (10)
             if (next == 'c') {
                 _round();
-                next = '\000';
+
             }
             else if (next == 'e') {
                 _historyCloser();
@@ -117,6 +109,7 @@ void _playerInput() {
             else{
                 printf("\nError please enter valid command; Enter 'c' to continue to the next round; else write 'e'...");
             }
+            next = '\000';
         }while (next != 'c');
 }
 
